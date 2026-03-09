2e313e01247c996cdfef01b58d937c649fbbc44a
smartgit707 <manmohan6145@gmail.com>
2026-03-09 09:08:33 +0530

    Add Use Case 1: Application entry point and welcome message

---
+++ src/UseCase1HotelBookingApp.java
@@ -0,0 +1,12 @@
+public class UseCase1HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("      Book My Stay Application   ");
+        System.out.println("    Hotel Booking System v1.0    ");
+        System.out.println("=================================");
+        System.out.println("Welcome to the Hotel Booking Management System!");
+
+    }
+}
